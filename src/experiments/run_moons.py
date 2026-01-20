ed3327e8471cf9bf23b7353f60dc1043d7634d3d
EduardoCavalcante <eduardocavalcante131@gmail.com>
2026-01-20 13:50:54 -0300

    feat: Run DBSCAN experiment on Two Moons dataset

--- src/experiments/run_moons.py
+++ src/experiments/run_moons.py
@@ -1,1 +1,19 @@
-# Run Two Moons experiment
+from src.algorithms.dbscan_manual import DBSCANManual
+from src.datasets.load_datasets import load_moons
+from src.visualization.plots import plot_dbscan_2d
+
+def run_moons():
+    X, _ = load_moons()
+
+    dbscan = DBSCANManual(eps=0.25, min_samples=5)
+    dbscan.fit(X)
+
+    plot_dbscan_2d(
+        X,
+        dbscan.point_types_,
+        title="DBSCAN Manual - Two Moons",
+        filename="two_moons_dbscan.png"
+    )
+
+if __name__ == "__main__":
+    run_moons()
