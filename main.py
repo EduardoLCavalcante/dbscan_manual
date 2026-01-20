9fea729425953e5c62df9f4b921f75c930a0bee1
EduardoCavalcante <eduardocavalcante131@gmail.com>
2026-01-20 13:50:56 -0300

    chore: Orchestrate experiments in main and add project cleanups

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# Main runner
+from src.experiments.run_moons import run_moons
+from src.experiments.run_circles import run_circles
+from src.experiments.run_iris import run_iris
+
+def main():
+    run_moons()
+    run_circles()
+    run_iris()
+
+if __name__ == "__main__":
+    main()
